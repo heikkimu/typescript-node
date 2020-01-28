9240b5f4c8deee74521b7e47f430cc29059093b2
Heikki Mustonen <heikki.mustonen@medisapiens.fi>
2020-01-28 19:27:54 +0200

    Add shared types

---
+++ shared/model/course-summary.ts
@@ -0,0 +1,7 @@
+export interface CourseSummary {
+    readonly url: string,
+    readonly description: string,
+    readonly iconUrl: string,
+    readonly courseListIcon: string,
+    seqNo: number
+}

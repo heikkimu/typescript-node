3fa6a21d550ff15dd580d21751a6110fb69dbba5
Heikki Mustonen <heikki.mustonen@medisapiens.fi>
2020-01-30 12:25:40 +0200

    Make queries and refine the REST-api

--- shared/model/course-detail.ts
+++ shared/model/course-detail.ts
@@ -1,7 +1,7 @@
 import { CourseSummary } from './course-summary';
 import { Lesson } from "./lesson";
 
-export interface Course extends CourseSummary{
+export interface CourseDetail extends CourseSummary{
     readonly longDescription: string,
     readonly comingSoon: boolean,
     readonly isNew?: boolean,
--- shared/model/course-summary.ts
+++ shared/model/course-summary.ts
@@ -1,7 +1,24 @@
 export interface CourseSummary {
+    readonly id: number,
     readonly url: string,
     readonly description: string,
     readonly iconUrl: string,
     readonly courseListIcon: string,
     seqNo: number
+}
+
+export function createCourseSummary({
+    id,
+    url,
+    description,
+    iconUrl,
+    courseListIcon,
+    seqNo }: any): CourseSummary {
+    return {
+        id, url, description, iconUrl, courseListIcon, seqNo
+    };
+}
+
+export function createCourseSummaries(data: any[]): CourseSummary[] {
+    return data.map(createCourseSummary)
 }
